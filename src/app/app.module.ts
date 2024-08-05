cbd2078a629e4269d1c825aebd1c9a9f00cfc808
Tommaso <t.mariotti.esterno@almaviva.it>
2024-08-05 17:12:51 +0200

    implementazione carosello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { FooterComponent } from './footer/footer.component';
+
+// Importa CarouselModule da @coreui/angular
+import { CarouselModule } from '@coreui/angular';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,14 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    MatCardModule  // Aggiungi CarouselModule agli import
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Aggiungi CUSTOM_ELEMENTS_SCHEMA
 })
 export class AppModule { }
