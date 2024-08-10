df1bbd9c0b7ae366066351749fa221e0636d97e9
Tommaso <t.mariotti.esterno@almaviva.it>
2024-08-10 18:26:27 +0200

    implementazione area riservata

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AreaRiservataComponent } from './area-riservata/area-riservata.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home-page',component:HomepageComponent},
+  {path:'area-riservata',component:AreaRiservataComponent},
+  { path: '**', redirectTo: 'home-page' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/area-riservata/area-riservata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-riservata',
+  standalone: true,
+  imports: [],
+  templateUrl: './area-riservata.component.html',
+  styleUrl: './area-riservata.component.css'
+})
+export class AreaRiservataComponent {
+
+}
