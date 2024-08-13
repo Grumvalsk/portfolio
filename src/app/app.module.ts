bbd596ea7ea4e8ea5c84d026a4c06c9c5223acb9
Tommaso <t.mariotti.esterno@almaviva.it>
2024-08-13 08:50:55 +0200

    implementazione fomr modifica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { FooterComponent } from './footer/footer.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AreaRiservataComponent } from './area-riservata/area-riservata.component';
 import { DettaglioEsperienzaComponent } from './dettaglio-esperienza/dettaglio-esperienza.component';
-import { MatFormFieldModule } from '@angular/material/form-field'; // Importa il componente
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; // Importa MatInputModule
 
 @NgModule({
   declarations: [
@@ -20,15 +21,16 @@ import { MatFormFieldModule } from '@angular/material/form-field'; // Importa il
     HomepageComponent,
     FooterComponent,
     AreaRiservataComponent,
-    DettaglioEsperienzaComponent, // Dichiara il componente qui
+    DettaglioEsperienzaComponent,
   ],
   imports: [
     AppRoutingModule,
     MatButtonModule,
     MatDialogModule,
-    MatCardModule, // Importa il modulo MatCard
+    MatCardModule,
     BrowserModule,
     MatFormFieldModule,
+    MatInputModule, // Aggiungi MatInputModule agli imports
     HttpClientModule
   ],
   bootstrap: [AppComponent],
