9ba3aca2f24d59c0b434fe894634a0a2a93bdd4d
Programmazione <mariotti.tommaso29@gmail.com>
2025-05-11 13:04:19 +0200

    impl oauth2 google

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,27 +1,46 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { User } from '../user';
+import { Oauth2Service } from '../oauth-2.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isShrunk = false;
   user: User = new User();
 
-  constructor(private rotte: Router, public dialog: MatDialog) {}
+  constructor(private rotte: Router, public dialog: MatDialog,private googleService:Oauth2Service,private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const access_token = params.get('access_token');
+      if(access_token){
+        const currentTime = new Date().getTime()+""; // ottieni il timestamp corrente
+        sessionStorage.setItem("token",access_token)
+        sessionStorage.setItem('exp',currentTime)
+      console.log('Parametro di rotta id:', access_token);
+      }
+    });  }
 
   areaRiservata() {
-    if (sessionStorage.getItem('Authorization')) {
-        this.rotte.navigate(['area-riservata']);
-    } else {
-      this.rotte.navigate(['login']);
-    }
+    const token=sessionStorage.getItem("token");
+    const exp=sessionStorage.getItem("exp")
+    if(!token&&!exp){
+    this.googleService.loginWithGoogle();
+     } else {
+      if(exp&&this.checkTokenValidity(exp)){
+       this.rotte.navigate(['area-riservata']);
+      }else{
+        this.googleService.loginWithGoogle();
+
+      }
+
   }
+}
 
   // Aggiunta la logica per gestire lo scroll
   @HostListener('window:scroll', [])
@@ -29,4 +48,22 @@ export class HeaderComponent {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     this.isShrunk = scrollPosition > 100;
   }
+
+  // Funzione per verificare la validità del token
+checkTokenValidity(exp:string): boolean {
+  if (exp) {
+    const currentTime = new Date().getTime(); // ora corrente
+    const elapsedTime = currentTime - parseInt(exp, 10); // differenza in millisecondi
+
+    // 1 ora = 3600000 millisecondi
+    if (elapsedTime < 3600000) {
+      return true; // Il token è ancora valido (meno di un'ora)
+    }else{
+      return false;
+    }
+  }
+  return false; // Token scaduto o assente
+}
+
+
 }
---
+++ src/app/oauth-2.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Oauth2Service {
+
+  constructor() { }
+
+  loginWithGoogle(): void {
+    const clientId = '841366050574-hd34fl2knl6aq416jrqln4dmo5ec20fj.apps.googleusercontent.com';
+    const redirectUri = 'https://portfoliogrumvalski.vercel.app/home';
+    const scope = 'email profile openid';
+    const responseType = 'token';
+    const state = crypto.randomUUID(); // oppure un tuo valore
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
+      `client_id=${clientId}` +
+      `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+      `&response_type=${responseType}` +
+      `&scope=${encodeURIComponent(scope)}` +
+      `&state=${state}`;
+
+    window.location.href = authUrl;
+  }
+
+}
